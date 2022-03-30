106c21136039b2f38de3e1c0960c7007a7f49139
Maryia Malakhava <malakhava@yahoo.com>
2022-03-30 18:13:44 -0400

    add and delete friend

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -72,6 +72,35 @@ const userController = {
       })
       .catch((err) => res.status(400).json(err));
   },
+
+   // add friend
+   addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.id },
+      { $addToSet: { friends: params.friendsId } },
+      { new: true }
+    )
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+   // remove friend
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.id },
+      { $pull: { friends: params.friendsId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "no user found with this ID" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  }
+
 };
 
 // esxport
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,6 +6,8 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  addFriend,
+  removeFriend
 } = require("../../controllers/user-controller");
 
 // Set up GET all and POST at /api/users
@@ -13,5 +15,8 @@ router.route("/").get(getAllUsers).post(createUser);
 
 // Set up GET one, PUT, and DELETE at /api/users/:id
 router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+// Add and delete a friend
+router.route("/:id/friends/:friendsId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
